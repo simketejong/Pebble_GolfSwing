806068852a331e998edfe5018daba36717423f91
simketejong <simketejong@gmail.com>
2016-07-04 15:30:39 +0200

    Coordinates errors 4

--- src/app.js
+++ src/app.js
@@ -14,11 +14,8 @@ var myError = 0;
 
 var main = new UI.Card({
   title: 'GolfSwing',
-/* icon: 'images/menu_icon.png', */
   subtitle: 'GolfSwing!',
   body: 'Shake to activate',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
 });
 
 /* GPS stuf */
@@ -44,7 +41,6 @@ function locationError(err) {
 // Make an asynchronous request
 navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
 Accel.init();
-main.show();
 /*Vibe.vibrate('short'); */
 /*main.on('accelTap', function(e) { */
 navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
@@ -52,74 +48,6 @@ navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locatio
 main.title('Position');
 main.subtitle('GPS');
 main.body('lat= ' + myLat + '\nlon= ' + myLong + '\nError: ' + myError);
+main.show();
 /*});*/
 
-/**main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
-*/
