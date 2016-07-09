8e9b83f05281cb34f479e7f90ccbcba4ce7d1dd2
simketejong <simketejong@users.noreply.github.com>
2016-07-09 09:55:12 +0200

    de laaste

--- src/app.js
+++ src/app.js
@@ -5,13 +5,21 @@
  */
 
 var UI = require('ui');
-/*var Vector2 = require('vector2');*/
 var Vibe = require('ui/vibe');
 var Accel = require('ui/accel');
+var ajax = require('ajax');
+
+var DATADROPURL = 'http://192.168.1.4/GolfClub.php?data=';
+
+/* Globals */
 var myLat = 0;
 var myLong = 0;
 var myError = 0;
-
+/*
+var Xaverage = 0;
+var Yaverage = 0;
+var Zaverage = 0;
+*/
 var main = new UI.Card({
   title: 'GolfSwing',
   subtitle: 'GolfSwing!',
@@ -40,6 +48,9 @@ function locationError(err) {
 
 // Make an asynchronous request
 navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+
+Accel.config({rate:10, samples:1, subscribe:true});
+
 Accel.init();
 main.show(); 
 main.on('accelTap', function(e) {
@@ -47,7 +58,40 @@ main.on('accelTap', function(e) {
     Vibe.vibrate('double');
     main.title('Position');
     main.subtitle('GPS');
-    main.body('lat= ' + myLat + '\nlon= ' + myLong + '\nError: ' + myError);
+    main.body('lat= ' + myLat + '\nlon= ' + myLong + '\nError: ' + myError);  
   }
 });
+    
+main.on('accelData', function(e) {
+main.subtitle(parseInt(Math.pow((Math.pow(e.accel.x,2) + Math.pow(e.accel.y,2) + Math.pow(e.accel.z,2)),0.5))); 
+main.body('x=' + e.accel.x + '\n' + 'y=' + e.accel.y + '\n' + 'z=' + e.accel.z + '\n' + 'vibe=' + e.accel.vibe + '\n' + 'time=' +   e.accel.time + '\n'); 
+  ajax(
+      {
+        url: DATADROPURL +    
+          '&x='+e.accel.x+
+          '&y='+e.accel.y+
+          '&z='+e.accel.z+
+          '&vibe='+e.accel.vibe+
+          '&time='+e.accel.time/*+
+          '&Xaverage'+Xaverage+
+          '&Yaverage'+Yaverage+
+          '&Zaverage'+Zaverage*/,
+          type:'json',
+          async:'true'
+      },
+      function(data){
+       console.log('Succeeded: ' + data);
+      },
+      function(error) {
+        console.log('Download failed: ' + error);
+      }
+    );
+/*  Xaverage=(Xaverage+e.accel.x)/2;
+  Yaverage=(Yaverage+e.accel.y)/2;
+  Zaverage=(Zaverage+e.accel.z)/2;*/
+});
+
+
+
+
 
